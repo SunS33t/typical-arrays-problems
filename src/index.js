d36128373f327accb9dce8323d133b9ac383daf1
SunS3t <deniska355466@gmail.com>
2020-10-03 15:39:44 +0400

    fixed names

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+let maixmum = Number.MIN_SAFE_INTEGER;
+let minimum = Number.MAX_SAFE_INTEGER;
 
-exports.min = function min (array) {
-  return 0;
+exports.min= function min (array) {
+  if(array.length == 0) return 0;
+  
+  array.forEach(element => {
+    if(element <= minimum){
+      minimum = element;
+    }
+  });
+  return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max= function max (array) {
+ if(array.length == 0) return 0;
+ array.forEach(element => {
+   if(element >= maixmum){
+     maixmum = element;
+   }
+ });
+ return maixmum;
 }
+
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array.length == 0) return 0;
+  let sum = 0;
+  array.forEach(element => {
+    sum+=element;
+  });
+  return sum / array.length;
 }
